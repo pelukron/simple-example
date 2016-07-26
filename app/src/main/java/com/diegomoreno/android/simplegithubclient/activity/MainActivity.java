23a02e2d307173cc8c6ac4c10ada9c683981e1da
dmoreno <diegohmore@hotmail.com>
2016-07-25 20:09:12 -0500

    A little change in the design

--- app/src/main/java/com/diegomoreno/android/simplegithubclient/activity/MainActivity.java
+++ app/src/main/java/com/diegomoreno/android/simplegithubclient/activity/MainActivity.java
@@ -1,16 +1,20 @@
 package com.diegomoreno.android.simplegithubclient.activity;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.diegomoreno.android.simplegithubclient.R;
 import com.diegomoreno.android.simplegithubclient.adapter.RepoAdapter;
 import com.diegomoreno.android.simplegithubclient.model.Repo;
 import com.diegomoreno.android.simplegithubclient.rest.ApiClient;
 import com.diegomoreno.android.simplegithubclient.rest.ApiInterface;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private String user;
     private RecyclerView recyclerView;
+    private ImageView userImage;
+    private TextView userNameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +36,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        user = "rubixware";
+        userImage = (ImageView) findViewById(R.id.user_image);
+        userNameText = (TextView) findViewById(R.id.user_name);
+        user = getResources().getString(R.string.user_name);
+        userNameText.setText(user);
         showData(user);
     }
 
-    private void showData (String name){
-
+    private void showData (final String name){
+        Picasso.with(getApplicationContext())
+                .load(getResources().getString(R.string.avatar_image)).into(userImage);
         ApiInterface apiInterface =
                 ApiClient.getRetrofit().create(ApiInterface.class);
         Call<List<Repo>> call = apiInterface.listRepos(name);
